fc9d371b7397a49fcc593f0f66e002169ef09a2a
EmreAydog <emreaydo@outlook.com>
2018-02-25 18:32:06 +0100

    Add files via upload

--- FrEmPlayer.java
+++ FrEmPlayer.java
@@ -7,16 +7,12 @@ import java.util.List;
 import nl.tue.s2id90.draughts.DraughtsState;
 import nl.tue.s2id90.draughts.player.DraughtsPlayer;
 import org10x10.dam.game.Move;
-import static java.lang.Math.max;
-import static java.lang.Math.min;
-
 
 /**
  * Implementation of the DraughtsPlayer interface.
  * @author huub
  */
-// ToDo: rename this class (and hence this file) to have a distinct name
-//       for your player during the tournament
+
 public class FrEmPlayer  extends DraughtsPlayer{
     private int bestValue=0;
     int maxSearchDepth;
@@ -28,10 +24,11 @@ public class FrEmPlayer  extends DraughtsPlayer{
         super("best.png"); // ToDo: replace with your own icon
         this.maxSearchDepth = maxSearchDepth;
     }
-
+    
     @Override public Move getMove(DraughtsState s) {
         Move bestMove = null;
         bestValue = 0;
+        
         DraughtsNode node = new DraughtsNode(s);    // the root of the search tree
         try {
             // compute bestMove and bestValue in a call to alphabeta
@@ -91,8 +88,13 @@ public class FrEmPlayer  extends DraughtsPlayer{
             throws AIStoppedException
     {
         if (node.getState().isWhiteToMove()) {
+            
+            // Maximizing player, hence we call alphaBetaMax in this loop
             return alphaBetaMax(node, alpha, beta, depth);
+            
         } else  {
+            
+            //Minimizing player, hence we call alphabetaMin in this loop
             return alphaBetaMin(node, alpha, beta, depth);
         }
     }
@@ -116,62 +118,99 @@ public class FrEmPlayer  extends DraughtsPlayer{
      */
      int alphaBetaMin(DraughtsNode node, int alpha, int beta, int depth)
             throws AIStoppedException {
-        if (stopped) { stopped = false; throw new AIStoppedException(); }
+         
+        if (stopped) { 
+            stopped = false; 
+            throw new AIStoppedException(); 
+        }
+        
         DraughtsState state = node.getState();
         
-        List<Move> moves = state.getMoves();
+        /** 
+         * Have we reached our maximum depth or are we in a leaf? Then evaluate
+         * the GameState and return its associated value.
+         */
+        if (depth <= 0 || state.isEndState()) {
+            return evaluate(state);
+        }
         
-        for (Move move : moves) {
+        for (Move move : state.getMoves()) {
             state.doMove(move);
-            System.out.println("BETA: " + beta);
             
-            // recursive call
-            // TODO clean up getting the minimal of the four
-            beta = evaluate(state);
-            System.out.println("New beta: " + beta);
-            beta = min(beta, min(alphaBetaMax(node, alpha, beta, depth), min(alpha, beta))); 
+            /**
+             * Part of the recursive call. 
+             * Here we move to the maximizing player while also decrementing
+             * the search depth by 1 to indicate that we have moved through a
+             * node in the tree. 
+             */
+            int temp = alphaBetaMax(node, alpha, beta, --depth);
             
-            if (beta <= alpha) {
-                return alpha;
+            // replace beta if we have found a smaller value for beta
+            if (temp < beta) {
+                beta = temp;
             }
             
             state.undoMove(move);
+            
+            // alpha beta pruning, return alpha if subtree can be discarded
+            if (beta <= alpha) {
+                return alpha;
+            }
+
         }
-        
-        Move bestMove = state.getMoves().get(0); // incomplete, needs evaluation
-        int value = evaluate(state);
+        Move bestMove = state.getMoves().get(0);
         node.setBestMove(bestMove);
-        return value;   // value of alpha
+        return beta;
      }
     
     int alphaBetaMax(DraughtsNode node, int alpha, int beta, int depth)
             throws AIStoppedException {
-        if (stopped) { stopped = false; throw new AIStoppedException(); }
+        
+        if (stopped) { 
+            stopped = false; 
+            throw new AIStoppedException(); 
+        }
+        
         DraughtsState state = node.getState();
         
-        List<Move> moves = state.getMoves();
+         /** 
+         * Have we reached our maximum depth or are we in a leaf? Then evaluate
+         * the GameState and return its associated value.
+         */
+        if (depth <= 0 || state.isEndState()) {
+            return evaluate(state);
+        }
         
-        for(Move move : moves) {
+        for (Move move: state.getMoves()) {
             state.doMove(move);
-            System.out.println("ALPHA: " + alpha);
-            // recursive call
-            // TODO clean up getting the maximal of the four
-            // use temp values for alpha???
-            alpha = evaluate(state);
-            System.out.println("New alpha: " + beta);
-            alpha = max(alpha, max(alphaBetaMin(node, alpha, beta, depth), max(alpha, beta)));
             
+            /**
+             * Part of the recursive call. 
+             * Here we move to the minimizing player while also decrementing
+             * the search depth by 1 to indicate that we have moved through a
+             * node in the tree. 
+             */
+            int temp = alphaBetaMin(node, alpha, beta, --depth);
+            
+            // replace alpha if we have found a larger value for alpha
+            if (temp > alpha) {
+                alpha = temp;
+            }
+            
+            state.undoMove(move);
+            
+            // alpha beta pruning, return beta if subtree can be discarded
             if (alpha >= beta) {
                 return beta;
             }
             
-            state.undoMove(move);
         }
-        
         Move bestMove = state.getMoves().get(0);
-        int value = evaluate(state);
         node.setBestMove(bestMove);
-        return value;
+        return alpha;
     }
+
+    /** A method that evaluates the given state. */
+    // ToDo: write an appropriate evaluation function
+    int evaluate(DraughtsState state) { return 0; }
 }
-    
