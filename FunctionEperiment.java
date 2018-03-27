64bcbf502301a51c8bb55feddd07d69eae4e796b
Hekker <f.e.j.hekker@student.tue.nl>
2018-03-27 20:34:14 +0200

    extra layers

--- FunctionEperiment.java
+++ FunctionEperiment.java
@@ -2,7 +2,9 @@ package experiments ;
 import nl.tue.s2id90.dl.experiment.Experiment ;
 import java.io.IOException ;
 import nl.tue.s2id90.dl.NN.Model;
+import nl.tue.s2id90.dl.NN.activation.RELU;
 import nl.tue.s2id90.dl.NN.initializer.Gaussian;
+import nl.tue.s2id90.dl.NN.layer.FullyConnected;
 import nl.tue.s2id90.dl.NN.layer.InputLayer;
 import nl.tue.s2id90.dl.NN.layer.SimpleOutput;
 import nl.tue.s2id90.dl.NN.loss.MSE;
@@ -12,24 +14,31 @@ import nl.tue.s2id90.dl.NN.tensor.TensorShape;
 import nl.tue.s2id90.dl.NN.validate.Regression;
 import nl.tue.s2id90.dl.input.GenerateFunctionData;
 import nl.tue.s2id90.dl.input.InputReader;
-import static org.apache.commons.math3.fitting.leastsquares.LeastSquaresFactory.model;
 public class FunctionEperiment extends Experiment {
 // ( hyper ) parameters
 int batchSize=32;
-int epochs=10;
-float learningRate=0.01f;
+int epochs=8;
+float learningRate=0.005f;
+int n=9;
+int layers=8;
+
 Model createModel ( int inputs , int outputs ) {
     Model model = new Model (new InputLayer ( " In " , new TensorShape ( inputs ) , true ) );
-    model.addLayer (new SimpleOutput ( "Out " , new TensorShape ( inputs ) , outputs , new MSE( ) , true ) ) ;
+    model.addLayer (new FullyConnected ( " f c 1 " , new TensorShape (inputs) , n , new RELU( ) ) ) ;
+    layers=layers-1;
+    for(int i=0;i<layers;i++){
+    model.addLayer (new FullyConnected ( " f c 1 " , new TensorShape (n) , n , new RELU( ) ) ) ;
+    }
+    model.addLayer (new SimpleOutput ( "Out " , new TensorShape ( n ) , outputs , new MSE( ) , true ) ) ;
     return model;
 }
 
 public void go ( ) throws IOException {
  // read input and pr int some informat ion on the data
  InputReader reader = GenerateFunctionData.THREE_VALUED_FUNCTION( batchSize ) ;
-int inputs = reader.getInputShape( ).getNeuronCount ( ) ;
- int outputs = reader.getOutputShape( ).getNeuronCount ( ) ;
  System.out.println( " Reader info :\n" + reader.toString( ) ) ;
+ int inputs = reader.getInputShape( ).getNeuronCount ( ) ;
+ int outputs = reader.getOutputShape( ).getNeuronCount ( ) ;
  Model model=createModel(inputs, outputs);
  model.initialize(new Gaussian());
  // Training : c r e a t e and conf igure SGD && t r a i n model
